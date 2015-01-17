17d8d77bf73846adbafd862c59bf84c9c49f4409
emcenrue <edward.mcenrue@gmail.com>
2015-01-17 04:10:22 -0500

    adding content_script comments

--- content_script.js
+++ content_script.js
@@ -1,3 +1,7 @@
+// content_script.js is loaded after the page is loaded, so jquery can be used.
+
+
+// Scrap the user ID from the Facebook banner
 $(document).ready(function() {
     if (!localStorage.getItem('fbUser')) {
         var fbUser = $("a[title='Profile']").attr('href');
@@ -5,3 +9,9 @@ $(document).ready(function() {
         localStorage.setItem('fbUser', fbUser);
     }
 });
+
+
+
+// Store all users from the current facebook.com/groups HH page 
+// & possibly use background.js callbacks to query the database for hovercards to create.
+// & remove all facebook hover cards, replacing them with our own jquery hovercards if they are a match
