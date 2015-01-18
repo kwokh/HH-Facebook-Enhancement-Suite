d2caf5c576b590ec6ab787c6361c47385896183a
emcenrue <edward.mcenrue@gmail.com>
2015-01-18 13:31:53 -0500

    update access

--- likes.js
+++ likes.js
@@ -1,4 +1,4 @@
-var access_token = 'CAACEdEose0cBAD49dt1Fn4C3f968au59xUhKem3BJIpZCTLf04GlfdYZB12qvZB9XbJUoySu9z3mEBN0auNwrDJRrLhsT36EGX3ed98v15nCi9ELkp8uRiivikkgtT08ZABdTC5MFQxACuf6PXLbtqoF4vw0SyM1J2KbJXG0ZCEPl0Q4tliXKm6khZAVaUWwKsZB1FgBxQSBApwJ8oENdA2';
+var access_token = 'CAACEdEose0cBAM1NZCTwthZAT7L1oZBVvY0RaG6JISwIj7K4k8TT3Qo8PyZAZAQDRd8SJ9Piw3r7OHxGg7bMCx9wXtdUVq3f5cU8xSQWaCZCg42lhuTUan1c0ljQ1I4owDSnRQspcDKkKWtTHt5d6CtehDUwV7siyaQxUIEohRWQoTl7HxdEGYHkTWJg8Ed1nKQeqqUa6WqNpsfvjTVyRc';
 $(document).ready(function() {
 	var likeList = JSON.parse(localStorage.getItem('likedLinks'));
 	$likes = $('#likes');
